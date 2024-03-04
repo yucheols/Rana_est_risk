cdab32d54c6805e7bc22376a6e47d85f9f8f9ef5
Yucheol Shin <brongersmai2@gmail.com>
2024-03-04 15:20:29 +0900

    commit

--- code/prelim_ENMs.R
+++ code/prelim_ENMs.R
@@ -1,14 +1,41 @@
 #####  load packages
 library(megaSDM)
 library(raster)
-library()
+library(plyr)
+library(dplyr)
+library(readr)
+library(SDMtune)
 
 # prevent encoding error
 Sys.getlocale()
 Sys.setlocale("LC_CTYPE", ".1251")
 Sys.getlocale()
 
-# Part 1 ::: data processing --------------------------------------------------------------------------------------------------------------
+##### Part 1 ::: envs data -----------------------------------------------------------------------------------------------------
+# process envs
+clim <- raster::stack(list.files(path = 'E:/env layers/worldclim', pattern = '.tif$', full.names = T))
+clim <- raster::crop(clim, extent(ch_poly))
+clim <- raster::mask(clim, ch_poly)
+
+topo <- raster('E:/env layers/elev_worldclim/wc2.1_30s_elev.tif')
+topo <- raster::crop(topo, extent(ch_poly))
+topo <- raster::mask(topo, ch_poly)
+
+land <- raster::stack(list.files(path = 'E:/env layers/land cover', pattern = '.tif$', full.names = T))
+land <- raster::stack(subset(land, c('cultivated', 'water')))
+land <- raster::crop(land, extent(ch_poly))
+land <- raster::mask(land, ch_poly)
+
+# stack env
+envs <- raster::stack(clim, topo, land)
+names(envs) = c('bio1', 'bio10', 'bio11', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19', 
+                'bio2', 'bio3', 'bio4', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'elev', 'cultivated', 'water')
+
+print(envs)
+plot(envs[[1]])
+
+
+##### Part 2 ::: occs data -----------------------------------------------------------------------------------------------------
 # import China polygon
 ch_poly <- rgdal::readOGR('E:/Asia_shp/Chinas/CHN_adm0.shp')
 
@@ -19,3 +46,58 @@ megaSDM::OccurrenceCollection(spplist = c('Rana amurensis', 'Rana chensinensis',
                               trainingarea = extent(ch_poly))
 
 # compile occs
+occs <- list.files(path = 'data/occs', pattern = '.csv', full.names = T) %>%
+  lapply(read_csv) %>%
+  rbind.fill %>%
+  dplyr::select(4,6,5)
+
+colnames(occs) = c('species', 'long', 'lat')
+head(occs)
+
+# sort per species
+unique(sort(occs[, 1]))
+
+amurensis <- occs %>% filter(species == 'Rana amurensis')
+chensinensis <- occs %>% filter(species == 'Rana chensinensis')
+coreana <- occs %>% filter(species == 'Rana coreana')
+dybowskii <- occs %>% filter(species == 'Rana dybowskii')
+huanrenensis <- occs %>% filter(species == 'Rana huanrenensis')
+kukunoris <- occs %>% filter(species == 'Rana kukunoris')
+
+### thin occurrence points
+# automate
+thinner <- function(occs_list, coords, env, x, y){
+  output <- list()
+  
+  for (i in 1:length(occs_list)) {
+    occs <- occs_list[[i]] %>% dplyr::select(2,3)
+    thin <- thinData(coords = occs, env = terra::rast(env), x = x, y = y, verbose = T, progress = T)
+    output[[i]] <- thin
+  }
+  
+  return(output)
+}
+
+# run function
+thin_occ <- thinner(occs_list = list(amurensis, chensinensis, coreana, dybowskii, huanrenensis, kukunoris),
+                    coords = list(amurensis, chensinensis, coreana, dybowskii, huanrenensis, kukunoris),
+                    env = envs, x = 'long', y = 'lat')
+
+print(thin_occ)
+
+# sort per species
+amurensis <- thin_occ[[1]]
+chensinensis <- thin_occ[[2]]
+coreana <- thin_occ[[3]]    # only 2 occs.....drop this sp from modeling
+dybowskii <- thin_occ[[4]]
+huanrenensis <- thin_occ[[5]]
+kukunoris <- thin_occ[[6]]
+
+# plot out occs
+plot(envs[[1]])
+
+points(amurensis, col = 'red')
+points(chensinensis, col = 'blue')
+points(dybowskii, col = 'green')
+points(huanrenensis, col = 'black')
+points(kukunoris, col = 'orange')
